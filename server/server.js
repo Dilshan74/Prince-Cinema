a60d512a8579589184c498ff20d9ac816addbbbc
Dilshan Chamodh <dilshan06akalanka2003@gmail.com>
2026-06-19 01:40:26 +0530

    server.js change

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
-import { clerkMiddleware } from '@clerk/express'
+//import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
 
@@ -18,7 +18,7 @@ await connectDB()
 //Middleware
 app.use(express.json())
 app.use(cors())
-app.use(clerkMiddleware())
+//app.use(clerkMiddleware())
 
 //API routes
 app.get('/', (req, res) => {
