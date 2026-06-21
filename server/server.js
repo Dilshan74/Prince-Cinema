13c7fd4beca2a90c35a4137e945d4024576d4686
Dilshan Chamodh <dilshan06akalanka2003@gmail.com>
2026-06-21 20:41:48 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
-import { clerkMiddleware } from '@clerk/express'
+//import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
 
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
   res.send('Server is Live!')
 })
 
-app.use('/api/inngest', serve({ client: inngest, functions }))
+app.use('/api/inngest', serve({ client: inngest, functions }));
 
 app.get('/health', (req, res) => {
   res.json({ status: 'ok', message: 'Backend is running' })
